bb8f81f0f8d8bfe15aa7d08449a762e0639825d7
MengStar <641510128@qq.com>
2018-04-05 02:41:30 +0800

    新增配置模块

--- mb-config/src/main/java/meng/xing/mbconfig/MbConfigApplication.java
+++ mb-config/src/main/java/meng/xing/mbconfig/MbConfigApplication.java
@@ -2,8 +2,10 @@ package meng.xing.mbconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class MbConfigApplication {
 
 	public static void main(String[] args) {
