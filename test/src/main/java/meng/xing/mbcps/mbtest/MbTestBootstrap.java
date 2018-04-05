5e1bd864bbbc9cafcb70e84ecb594fe0ee7083f4
MengStar <641510128@qq.com>
2018-04-05 22:04:15 +0800

    增加docker支持

--- test/src/main/java/meng/xing/mbcps/mbtest/MbTestBootstrap.java
+++ test/src/main/java/meng/xing/mbcps/mbtest/MbTestBootstrap.java
@@ -1,4 +1,4 @@
-package a;
+package meng.xing.mbcps.mbtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 
-public class MbConfigBootstrap {
+public class MbTestBootstrap {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MbConfigBootstrap.class, args);
+		SpringApplication.run(MbTestBootstrap.class, args);
 	}
 }
