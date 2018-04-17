2248b8fb3fe108ccbacbd8366af929215c956eaf
mengstar <641510128@qq.com>
2018-04-17 22:35:48 +0800

    在客户端不启动base.yml配置的docker

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo `docker network create -d overlay springcloud-overlay` &&
-echo `docker network create -d overlay data-overlay` &&
+#echo `docker network create -d overlay data-overlay` &&
 cd docker &&
-docker stack deploy -c base.yml base           &&
+#docker stack deploy -c base.yml base      &&
 docker stack deploy -c center.yml center   &&
 docker stack deploy -c config.yml config   &&
 cd ../ &&
