639403647755fc84223f30e16cb5ed0003507cf8
mengstar <641510128@qq.com>
2018-04-17 15:06:31 +0800

    增加网络配置

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo `docker network create -d overlay springcloud-overlay` &&
+echo `docker network create -d overlay data-overlay` &&
 cd docker &&
 docker stack deploy -c base.yml base           &&
 docker stack deploy -c center.yml center   &&
