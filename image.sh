71c4c1e9b950bf588729f0054d374295f3875aa4
MengStar <641510128@qq.com>
2018-05-12 20:22:28 +0800

    修复

--- image.sh
+++ image.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-docker pull registry.cn-hangzhou.aliyuncs.com/mengstar/mongo:latest &&
-docker pull registry.cn-hangzhou.aliyuncs.com/mengstar/redis:latest &&
-docker pull registry.cn-hangzhou.aliyuncs.com/mengstar/mysql:latest &&
-docker pull registry.cn-hangzhou.aliyuncs.com/mengstar/rabbitmq:latest &&
+#docker pull registry.cn-hangzhou.aliyuncs.com/mengstar/mongo:latest &&
+#docker pull registry.cn-hangzhou.aliyuncs.com/mengstar/redis:latest &&
+#docker pull registry.cn-hangzhou.aliyuncs.com/mengstar/mysql:latest &&
+#docker pull registry.cn-hangzhou.aliyuncs.com/mengstar/rabbitmq:latest &&
 cd mb-center && mvn clean package docker:build && cd ../ &&
 cd mb-config && mvn clean package docker:build && cd ../ &&
-
+cd mb-gateway && mvn clean package docker:build && cd ../ &&
 docker images
