4f8b3cd0e57735434a02c57015f65536c8711dfb
MengStar <641510128@qq.com>
2018-05-15 11:34:07 +0800

    cache

--- mb-user/src/main/java/meng/xing/user/service/RoleService.java
+++ mb-user/src/main/java/meng/xing/user/service/RoleService.java
@@ -2,10 +2,12 @@ package meng.xing.user.service;
 
 import meng.xing.user.entity.Role;
 import org.springframework.stereotype.Service;
+import springfox.documentation.annotations.Cacheable;
 
 import java.util.Set;
 
 @Service
 public interface RoleService {
+     @Cacheable(value = "userCache")
      Set<Role> roles();
 }
