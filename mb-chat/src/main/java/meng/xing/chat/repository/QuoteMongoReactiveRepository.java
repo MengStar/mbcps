1558d1e0fc8cc476d8204e97b96e68cef1be7e7b
MengStar <641510128@qq.com>
2018-05-16 23:40:30 +0800

    增加mongo

---
+++ mb-chat/src/main/java/meng/xing/chat/repository/QuoteMongoReactiveRepository.java
@@ -0,0 +1,16 @@
+package meng.xing.chat.repository;
+
+
+import meng.xing.chat.entity.Quote;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface QuoteMongoReactiveRepository extends ReactiveCrudRepository<Quote, String> {
+
+    @Query("{ id: { $exists: true }}")
+    Flux<Quote> retrieveAllQuotesPaged(final Pageable page);
+}
